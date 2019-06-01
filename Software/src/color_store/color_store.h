073859f995ed7f73b18c1c2733210d9bb91dcc5f
Unknown <scherermarcel84@gmail.com>
2019-06-01 12:59:46 +0200

    add android SDK

--- Software/src/color_store/color_store.h
+++ Software/src/color_store/color_store.h
@@ -7,8 +7,17 @@ typedef struct
   uint8_t blue;
 } led_col_t;
 
+/* init EEPROM */
 void init_eeporm(void);
+
+/* load EEPROM value for led on */
 led_col_t init_value_on(void);
+
+/* load EEPROM value for led off */
 led_col_t init_value_off(void);
+
+/* store EEPROM value for led on */
 void set_value_on(led_col_t temp_col_on);
+
+/* store EEPROM value for led off */
 void set_value_off(led_col_t temp_col_off);
--- Software/src/word_array_hdl/word_array_hdl.h
+++ Software/src/word_array_hdl/word_array_hdl.h
@@ -3,7 +3,10 @@
 
 #include "../time_hdl/time_hdl.h"
 
+/* get the pointer of the led pixel array */
 uint8_t * create_word_array(clock_time_t actaul_time);
+
+/* return if the display shold be off or on depend on config file */
 bool check_if_display_on(clock_time_t actaul_time);
 
 #endif
